a7b1e0833971e421debef428386ceeec385f93aa
vamsichinnu <vamsichinnu@missionrnd.com>
2016-03-12 20:21:33 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -21,6 +21,22 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 
 
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
-
+	int i;
+	*consonants = 0;
+	*vowels = 0;
+	if (str)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (str[i] == 65 || str[i] == 69 || str[i] == 73 || str[i] == 79 || str[i] == 85 || str[i] == 97 || str[i] == 101 || str[i] == 105 || str[i] == 111 || str[i] == 117)
+			{
+				(*vowels)++;
+			}
+			else if ((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
+			{
+				(*consonants)++;
+			}
+		}
+	}
 
 }
